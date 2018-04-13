6d5045173acf94cd2a89bc9d464490d718e35590
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-04-13 14:28:19 +0200

    Implémentation de la classe TestGroup qui correspond au composite

--- code/include/shape/ShapeInterface.hpp
+++ code/include/shape/ShapeInterface.hpp
@@ -12,7 +12,9 @@ namespace shape
 
     virtual void draw () = 0;
 
-    virtual void resize (double size_coeff) = 0;
+    virtual void translate () = 0;
+
+    virtual void rotate () = 0;
 
     virtual bool operator== (const ShapeInterface& shape) = 0;
   };
--- code/src/shape/ShapeGroup.cpp
+++ code/src/shape/ShapeGroup.cpp
@@ -1,7 +1,62 @@
 #include "ShapeGroup.hpp"
 
+#include <algorithm>
+
 namespace shape
 {
   ShapeGroup::ShapeGroup () {}
   ShapeGroup::~ShapeGroup () {}
+
+  void ShapeGroup::add_shape (ShapeInterface* shape)
+  {
+    bool found =
+     (std::find (m_group.begin (), m_group.end (), shape) != m_group.end ());
+    if (!found)
+      m_group.push_back (shape);
+  }
+
+  void ShapeGroup::remove_shape (ShapeInterface* shape)
+  {
+    int cpt = 0;
+    for (const auto& it : m_group)
+    {
+      if (it == shape)
+      {
+        m_group.erase (m_group.begin () + cpt);
+        break;
+      }
+      ++cpt;
+    }
+  }
+
+  void ShapeGroup::draw ()
+  {
+    for (const auto& it : m_group)
+      it->draw ();
+  }
+
+  void ShapeGroup::translate ()
+  {
+    for (const auto& it : m_group)
+      it->translate ();
+  }
+
+  void ShapeGroup::rotate ()
+  {
+    for (auto const& it : m_group)
+      it->rotate ();
+  }
+
+  bool ShapeGroup::operator== (const ShapeGroup& group)
+  {
+    if (m_group.size () != group.m_group.size ())
+      return false;
+
+    for (unsigned int i = 0; i < m_group.size (); ++i)
+    {
+      if (m_group [i] != group.m_group [i])
+	return false;
+    }
+    return true;
+  }
 }
