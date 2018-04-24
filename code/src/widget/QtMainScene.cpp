e74dd68730098e6d3bde81ec35a1b13364150a84
Probak <probak@DebienIlBugMoins.Ma>
2018-04-24 19:37:54 +0200

    correction du drag & drop

--- code/src/widget/QtMainScene.cpp
+++ code/src/widget/QtMainScene.cpp
@@ -13,36 +13,22 @@ namespace widget
 
   void QtMainScene::mousePressEvent (QGraphicsSceneMouseEvent* mouseEvent)
   {
-    if (mouseEvent->button () == Qt::RightButton)
-      std::cout << "lol" << std::endl;
-    else // Propagation of the event.
-    {
       mouseEvent->ignore ();
       QGraphicsScene::mousePressEvent (mouseEvent);
-    }
   }
   void QtMainScene::mouseReleaseEvent (QGraphicsSceneMouseEvent* mouseEvent)
   {
-    if (mouseEvent->button () != Qt::RightButton)
-    {
       mouseEvent->ignore ();
       QGraphicsScene::mouseReleaseEvent (mouseEvent);
-    }
   }
   void QtMainScene::mouseMoveEvent (QGraphicsSceneMouseEvent* mouseEvent)
   {
-    if (mouseEvent->button () != Qt::RightButton)
-    {
       QGraphicsScene::mouseMoveEvent (mouseEvent);
       mouseEvent->ignore ();
-    }
   }
   void QtMainScene::mouseDoubleClickEvent (QGraphicsSceneMouseEvent* mouseEvent)
   {
-    if (mouseEvent->button () != Qt::RightButton)
-    {
       QGraphicsScene::mouseDoubleClickEvent (mouseEvent);
       mouseEvent->ignore ();
-    }
   }
 }
